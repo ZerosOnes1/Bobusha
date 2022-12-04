7edfa40330dd4a67a3e92e1d35e6711dca1e0df3
ZerosOnes <misha.makarenkov2282yandex.ru>
2022-12-04 23:15:32 +0700

    rekmaincpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 ﻿#include <iostream>
 
 using namespace std;
-double my_pow(double a, unsigned int b) {
-    double u = 1;
-    for (unsigned int i = 0; i < b; i++) {
-        u = u * a;
-    }
-    return u;
+double my_pow(double a, unsigned int b, double u) {
+	if (b == 0)
+	{
+		return u;
+	}
+	else
+	{
+		u = my_pow(a,b-1,u);
+		return u*a;
+	}
 }
 
 int main()
 {
+    double u = 1;
     double chislo;
     unsigned int stepen;
     cin >> chislo >> stepen;
-    cout<< my_pow(chislo, stepen);
+    cout << my_pow(chislo, stepen, u);
     return 0;
 }
