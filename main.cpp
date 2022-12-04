8f216949b01c6694b6e3961d3eb3ff17214014f6
ZerosOnes <misha.makarenkov2282yandex.ru>
2022-12-04 22:47:25 +0700

    maincpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+double my_pow(double a, unsigned int b) {
+    double u = 1;
+    for (unsigned int i = 0; i < b; i++) {
+        u = u * a;
+    }
+    return u;
+}
+
+int main()
+{
+    double chislo;
+    unsigned int stepen;
+    cin >> chislo >> stepen;
+    cout<< my_pow(chislo, stepen);
+    return 0;
+}
